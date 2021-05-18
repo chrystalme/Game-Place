c896b26f92e322e90b3b335bc83681691b7a27e6
chrystalme <afam_ifeanyi@live.com>
2021-05-18 15:20:35 +0300

    Add logo images

--- src/index.js
+++ src/index.js
@@ -23,13 +23,14 @@ const config = {
 const game = new Phaser.Game(config);
 
 let platforms;
+let player;
 
 function preload() {
   this.load.image('sky', '../assets/sky.png');
   this.load.image('ground', '../assets/platform.png');
   this.load.image('star', '../assets/star.png');
   this.load.image('bomb', '../assets/bomb.png');
-  this.load.spritesheet('nora', '../assets/nora01.png', { frameWidth: 32, frameHeight: 48 });
+  this.load.spritesheet('nora', '../assets/nora01.png', { frameWidth: 18, frameHeight: 48 });
 }
 
 function create() {
@@ -39,7 +40,31 @@ function create() {
   platforms.create(600, 400, 'ground');
   platforms.create(50, 250, 'ground');
   platforms.create(750, 220, 'ground');
-  platforms.create(450, 50, 'ground');
+  platforms.create(450, 50, 'ground').setScale(0.5);
+
+  player = this.physics.add.sprite(120, 430, 'nora');
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('nora', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: 'turn',
+    frames: [{ key: 'nora', frame: 4 }],
+    frameRate: 20,
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('nora', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
+  });
 }
 
 function update() {
