57654e0d635ceccc191448473e5f1bdd7a830d22
chrystalme <afam_ifeanyi@live.com>
2021-05-18 11:19:50 +0300

    Add basic structure to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('Hello World!');
+import Phaser from '../js/phaser';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+
+  }
+};
+
+const game = new Phaser.Game(config);
+
+const preload = () => {
+
+};
+
+const create = () => {
+
+};
+
+const update = () => {
+
+};
