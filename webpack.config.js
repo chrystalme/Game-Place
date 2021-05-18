1307f863f855ffa5b40b2cd22be25127d558391f
chrystalme <afam_ifeanyi@live.com>
2021-05-18 10:28:26 +0300

    Add loaders to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,9 @@
 
 const path = require('path');
 
-const isProduction = process.env.NODE_ENV == 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
+const stylesHandler = 'style-loader';
 
 const config = {
     entry: './src/index.js',
