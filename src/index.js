7aee589c1747745ad21ad3e07e1bdec88a119af0
chrystalme <afam_ifeanyi@live.com>
2021-05-18 12:04:22 +0300

    Add game assets and styling

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 import Phaser from '../js/phaser';
+import '../style/style.scss';
 
 const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   scene: {
-    preload: preload,
-    create: create,
-    update: update
+    preload,
+    create,
+    update,
 
-  }
+  },
 };
 
 const game = new Phaser.Game(config);
 
 const preload = () => {
-
+  this.load.image('sky', 'assets/sky.png');
+  this.load.image('ground', 'assets/platform.png');
+  this.load.image('star', '../assets/star.png');
+  this.load.image('bomb', '../assets/bomb.png');
+  this.load.spritesheet('nora', '../assets/nora01.png', { frameWidth: 32, frameHeight: 48 });
 };
 
 const create = () => {
