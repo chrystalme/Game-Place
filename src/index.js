503e2f86bdc3b7d8b77644f5b721df08bf97e0a8
chrystalme <afam_ifeanyi@live.com>
2021-05-18 13:53:48 +0300

    Add background and static elements

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import Phaser from '../js/phaser';
 import '../style/style.scss';
+import Phaser from '../js/phaser';
 
 const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
   scene: {
     preload,
     create,
@@ -15,18 +22,26 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-const preload = () => {
-  this.load.image('sky', 'assets/sky.png');
-  this.load.image('ground', 'assets/platform.png');
+let platforms;
+
+function preload() {
+  this.load.image('sky', '../assets/sky.png');
+  this.load.image('ground', '../assets/platform.png');
   this.load.image('star', '../assets/star.png');
   this.load.image('bomb', '../assets/bomb.png');
   this.load.spritesheet('nora', '../assets/nora01.png', { frameWidth: 32, frameHeight: 48 });
-};
+}
 
-const create = () => {
+function create() {
+  this.add.image(0, 0, 'sky').setOrigin(0, 0);
+  platforms = this.physics.add.staticGroup();
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
+  platforms.create(450, 50, 'ground');
+}
 
-};
+function update() {
 
-const update = () => {
-
-};
+}
