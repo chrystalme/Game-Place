a243f32572736606cdb5661f349100d64e8d592f
chrystalme <afam_ifeanyi@live.com>
2021-05-25 13:01:05 +0300

    Add api to post and get data

--- src/api.js
+++ src/api.js
@@ -1,40 +1,50 @@
+const score = () => {
+  const key = 'w2QrElL4pZZ70MTwdThv';
+  const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${key}/scores/`;
+  const data = {};
 
-const key = 'w2QrElL4pZZ70MTwdThv';
-const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${key}/scores/`;
+  const postData = async () => {
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data),
+      });
 
-const { score } = this.game.global;
+      return response.json();
+    } catch (error) {
+      throw new Error(`API could not be reached: ${error}`);
+    }
+  };
 
-const data = { user: name, score };
+  const getData = async () => {
+    try {
+      const response = await fetch(url, {
+        mode: 'cors',
+      });
+      return response.json();
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
 
-const postData = async (url = '', data = {}) => {
-  // Default options are marked with *
-  try {
-    const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'application/json',
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data), // body data type must match "Content-Type" header
-    });
+  const setName = (name) => {
+    data.user = name;
+  };
 
-    return response.json(); // parses JSON response into native JavaScript objects
-  } catch (error) {
-    throw new Error(`API could not be reached: ${error}`);
-  }
+  const setScore = (score) => {
+    data.score = score;
+  };
+  return {
+    postData, getData, setName, setScore,
+  };
 };
 
-const getData = async (url = '') => {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    mode: 'cors', // no-cors, *cors, same-origin
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-};
-
-export default { postData, getData };
+export default score;
